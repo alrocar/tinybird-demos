a223ed86bfa40d55d1b73183a17d5ee7386bd0e1
alrocar <alrocar@tinybird.co>
2022-02-05 20:23:56 +0100

    tt

--- avatar/cronjob.py
+++ avatar/cronjob.py
@@ -2,6 +2,6 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from avatar import run
 
 scheduler = BlockingScheduler()
-scheduler.add_job(run, "interval", seconds=10)
+scheduler.add_job(run, "interval", seconds=300)
 
 scheduler.start()
